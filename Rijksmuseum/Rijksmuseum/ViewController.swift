d6ebb54b3f827d988c95d31e8d79b6006fe59ae0
ytroyan <troyanyoura@gmail.com>
2023-05-28 13:23:29 +0200

    add empty xcode project

---
+++ Rijksmuseum/Rijksmuseum/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rijksmuseum
+//
+//  Created by Troian on 28.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
